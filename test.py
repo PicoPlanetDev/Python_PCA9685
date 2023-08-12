2c55e305be9a485be1b8196514eaffe229d097f6
Sigmond Kukla <picoplanetdev@gmail.com>
2023-08-11 23:51:50 -0400

    Test appears to work

--- test.py
+++ test.py
@@ -5,14 +5,16 @@ import time
 pwm = PCA9685() # use default I2C bus 3, default address 0x40
 pwm.set_pwm_freq(50) # set frequency to 50 Hz for SG90 servos
 
-# set servo to neutral position
-pwm.set_pwm(0, 0, 375) # channel 0, off = 375
-time.sleep(1) # wait for servo to move
+print("Press Ctrl+C to quit...")
+while True:
+    # set servo to -90 degrees
+    pwm.set_pwm(0, 0, 102) # channel 0, off time 102 (1 ms)
+    time.sleep(1)
 
-# set servo to 90 degrees
-pwm.set_pwm(0, 0, 225) # channel 0, off = 225
-time.sleep(1) # wait for servo to move
+    # set servo to 0 degrees
+    pwm.set_pwm(0, 0, 307) # channel 0, off time 307 (1.5 ms)
+    time.sleep(1)
 
-# set servo to 180 degrees
-pwm.set_pwm(0, 0, 75) # channel 0, off = 75
-time.sleep(1) # wait for servo to move
+    # set servo to 90 degrees
+    pwm.set_pwm(0, 0, 512) # channel 0, off time 512 (2 ms)
+    time.sleep(1)
